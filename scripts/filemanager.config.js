90a87f59bbbb786975fa7ab4ef6083090316b114
Richard <radamca@gsi.de>
2015-07-09 11:06:41 +0200

    config in filemanager

--- scripts/filemanager.config.js
+++ scripts/filemanager.config.js
@@ -19,7 +19,7 @@
         "dateFormat": "d M Y H:i",
         "serverRoot": true,
         "fileRoot": "uploads/_USER/",
-        "baseUrl": "https://kurier-dev.gsi.de/",
+        "baseUrl": "https://kurier-dev.gsi.de/uploads/_USER/",
         "logger": false,
         "capabilities": ["select", "download", "rename", "delete", "replace"],
         "plugins": []
